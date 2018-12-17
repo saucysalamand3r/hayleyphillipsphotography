dfecdd11e12049a03d9e6fc5bceddc0065a1af02
Nate Wolfe <natedwolfe@gmail.com>
2018-12-17 14:45:18 -0500

    Switched to dropdown main menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,14 +2,14 @@ import React from "react"
 import './card.css'
 
 const Menu =()=> (
-  <section class="card menu">
+  <div class="card menu">
     <ul>
         <li><a>Portfolio</a></li>
         <li><a>Packages</a></li>
         <li><a>Get To Know Me</a></li>
         <li><a>Contact</a></li>
     </ul>
-  </section>
+  </div>
 )
 
 export default Menu
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,20 @@ import SEO from '../components/seo';
 import Card from '../components/card';
 import Menu from '../components/menu';
 import Social from '../components/social';
+import Dropdown from '../components/dropdown';
 
 const divStyle = {
   display: 'flex',
+  maxWidth: '100vw',
+  flexDirection: 'column',
 }
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
     <div style={divStyle}>
-      <Menu></Menu>
+      <Dropdown></Dropdown>
+      {/* <Menu></Menu> */}
       <Card>
         <p>Hi, I'm Hayley Phillips, welcome to my Photography page! Feel free to explore! On the left, you will find a directory menu laid out to help you find what you're looking for. I hope you love what you see as much as I do, because this is not just my business; it is my art and passion. If you have any questions, feel free to email at <a href="mailto://hbphillips@gmail.com">hpphillips99@gmail.com</a>!</p>
       </Card>
